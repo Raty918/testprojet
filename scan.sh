6b10e141a4e6fdcb17ba3eb826e732d3ed56fdaa
Raty918 <raty918@gmail.com>
2017-06-29 18:33:45 +0200

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -19,7 +19,7 @@ if [ "$10" = true ];
         then
 		# To skip crawling and only audit elements discovered by using the proxy
 		# the scope page-limit option is set to '0'.
-       		SCAN_OPTIONS="$SCAN_OPTIONS --plugin=proxy:port=8282,bind_address=127.0.0.1 "
+       		SCAN_OPTIONS="$SCAN_OPTIONS --plugin=proxy:port=8282,bind_address=0.0.0.0 "
 fi
 
         
@@ -27,10 +27,10 @@ fi
 if [ "$1" != "default" ]; #DISPATCHER_GRID_HOST not set
         then
 		( exec "./wait-for-it.sh -t 0 $1" )	
-                echo "docker -H $2 exec -t v4_web_1 bin/arachni_rpc --dispatcher-url $1 $SCAN_OPTIONS"   
+               docker -H $2 exec -t v4_web_1 bin/arachni_rpc --dispatcher-url $1 $SCAN_OPTIONS"   
         else  
                 add=$(docker -H $2 inspect v4_dispatcher_grid_1 | grep IPAddress | cut -d '"' -f 4):7331
                 ( exec "./wait-for-it.sh -t 0 $add" )	
-	       echo "docker -H $2 exec -t v4_web_1 bin/arachni_rpc --dispatcher-url $add $SCAN_OPTIONS"
+	       docker -H $2 exec -t v4_web_1 bin/arachni_rpc --dispatcher-url $add $SCAN_OPTIONS"
 fi
 
